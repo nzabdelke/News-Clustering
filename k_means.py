f8239e2b4313e6a89c88fa4b67359c49b56cfd7b
zhnabdelke <zhnabdelke@gmail.com>
2021-06-07 15:54:19 -0400

    add KMeans class

--- k_means.py
+++ k_means.py
@@ -6,3 +6,19 @@ from numpy.linalg import norm
 def cosine_distance(vector1, vector2):
 
     return 1 - (dot(vector1, vector2) / (norm(vector1) * norm(vector2)))
+
+
+class KMeans:
+    def __init__(self, k, iterations):
+
+        # k is the number of clusters
+        self.k = k
+
+        # iterations is one of the termination criteria
+        self.iterations = iterations
+
+        # initialize the centroids as an empty list
+        self.centroids = []
+
+        # initialize each cluster as an empty list
+        self.clusters = [[] for i in range(self.k)]
